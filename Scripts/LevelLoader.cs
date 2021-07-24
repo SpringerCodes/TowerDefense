fae903f4d22185bdf2c86258513d71adcb05fcb3
Chris Springer <topher.springer1@gmail.com>
2021-07-24 10:04:48 -0700

    Add files via upload

---
+++ Scripts/LevelLoader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 2;
+    int currentSceneIndex;
+    
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+    public void RestartScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+        Time.timeScale = 1;
+    }
+
+    public void LoadOptionsScreen()
+    {
+        SceneManager.LoadScene("Options Screen");        
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadYouLose()
+    {
+        SceneManager.LoadScene("Lose Screen");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
